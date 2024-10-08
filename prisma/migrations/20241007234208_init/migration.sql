f7447848cdee84110db86cb7e83057d671e4f974
Leone-H <hotroddustbit@gmail.com>
2024-10-08 03:05:03 +0300

    database dummy added

---
+++ prisma/migrations/20241007234208_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "NewsPost" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "article" TEXT NOT NULL
+);
