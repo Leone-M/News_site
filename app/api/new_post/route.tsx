ff6a544eebf84ab8fa29af6d485a8ded1b13ea9e
Leone-H <hotroddustbit@gmail.com>
2024-10-09 21:56:04 +0300

    Ура победа...

--- app/api/new_post/route.tsx
+++ app/api/new_post/route.tsx
@@ -3,36 +3,59 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from "@/app/lib/script"
 import { consumers } from 'stream';
 
-export async function string_parser(){
-
+export async function string_parser(content_type: string, parsed_body: string){
+  content_type = "--" + content_type.replace("multipart/form-data; boundary=", "");
+  let blocks: string[] = parsed_body.split(content_type);
+  blocks = blocks.slice(2, -1)
+  const parsed_blocks: string[] = [];
+  blocks.forEach(block => {
+    block = block.replaceAll("--", "")
+    block = block.replaceAll("\n", "")
+    block = block.trim()
+    block = block.replace("Content-Disposition: form-data; name=", "").replace("\"", "").replace("\"", "").replace("\r", "")
+    parsed_blocks.push(block)
+  });
+  const keys: string[] = [];
+  const values: string[] = [];
+  parsed_blocks.forEach(block =>{
+    let bufarr: string[] = [];
+    bufarr = block.split("\r");
+    keys.push(bufarr[0]);
+    values.push(bufarr[1]);
+  })
+  var data_base_ready_dict = new Object();
+  for (let i = 0; i < keys.length; i++) {
+    data_base_ready_dict[keys[i]] = values[i];
+  }
+  await prisma.newsPost.create({
+    data: data_base_ready_dict
+  })
 }
 
 export async function POST(request) {
-  console.log(request.headers)
-  console.log(request.headers.get("Content-type"));
+  console.log("REQUESTING POST");
   const stream = request.body;
-  const reader = stream.getReader();
-  var parsed_string: string = "";
+  const content_type = request.headers.get("content-type")
+  // const reader = stream.getReader();
   let chunks = [], decoder = new TextDecoder();
-  reader.read().then(function processData({done, value}) {
-    if (done) {
-      console.log(decoder.decode(concat(chunks)));
-      return;
-    }
-    chunks.push(value);
-    reader.read().then(processData);
-  })
-
-  function concat(chunks) {
-    return chunks.reduce((acc, chunk) => new Uint8Array([...acc, ...chunk]), new Uint8Array());
+  for await (const chunk of request.body){
+    chunks.push(decoder.decode(chunk))
   }
+  const parsed_string: string = chunks[0]
+  // parsed_string = reader.read().then(function processData({done, value}) {
+  //   if (done) {
+  //     let parsed_string = (decoder.decode(concat(chunks)));
+  //     return;
+  //   }
+  //   chunks.push(value);
+  //   reader.read().then(processData);
+  // })
 
-  // const raw_form_data = {
-  //   title: req_parsed.get("title"),
-  //   image_url: req_parsed.get("image_url"),
-  //   article: req_parsed.get("article")
+  // function concat(chunks) {
+  //   return chunks.reduce((acc, chunk) => new Uint8Array([...acc, ...chunk]), new Uint8Array());
   // }
-  console.log(parsed_string)
+  string_parser(content_type, parsed_string)
+  console.log("FINISHED REQUESTING")
   return new Response()
 }
 
