1b1cf8dad4a720288c7936567030603689dc2fdd
Leone-H <hotroddustbit@gmail.com>
2024-10-10 01:13:14 +0300

    added to gitignore

--- prisma/migrations/20241007234208_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "NewsPost" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "image_url" TEXT NOT NULL,
-    "article" TEXT NOT NULL
-);
